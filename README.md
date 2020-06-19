27b9433bc1c35f07216d8d464a8ecb96dd35e60e
Preeti Gupta <preetigupta510@gmail.com>
2020-06-18 22:59:58 -0700

    adding the logic and readme file

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Password-_Generator
+# Portfolio Homework
+The purpose of this assignment was to generate password with different sets of requirement.
+ 
+
+## Getting Started
+
+### Prerequisites
+1. *Javascript*
+
+2. *HTML*
+
+3. *CSS*
+
+My url Link ->  (https://preeti1234567.github.io/Portfolio_Homework/)
+
--- script.js
+++ script.js
@@ -1,46 +1,68 @@
 // Write password to the #password input
 //actual logic for password generator
-function getNumber(min,max){
-  return Math.floor(Math.random() * (max - min) ) + min;
+function getNumber(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
 }
 
-function generatePassword(length) {
+function generatePassword(length, upperCase, lowerCase, number, specialChar) {
   var arr = [];
   var counter = 0;
-  //Upper Case
-  for(var i = 0; i < length; i++)
-    arr[counter++] = String.fromCharCode(getNumber(65,90));
-  //Lower Case
-  for(var i = 0; i < length; i++)
-    arr[counter++] = String.fromCharCode(getNumber(97,122));
-  //Number
-  for(var i = 0; i < 2; i++)
-    arr[counter++] = String.fromCharCode(getNumber(48,57));
-  //Special Character
-  for(var i = 0; i < 2; i++)
-    arr[counter++] = String.fromCharCode(getNumber(33,47));
+  var requirementCount = 0;
+  if (upperCase)
+    requirementCount++;
+  if (lowerCase)
+    requirementCount++;
+  if (number)
+    requirementCount++;
+  if (specialChar)
+    requirementCount++;
+
+  var requirementLength = length / requirementCount;
+
+  while (counter < length) {
+    //Upper Case
+    if (upperCase) {
+      for (var i = 0; i < requirementLength && counter < length; i++)
+        arr[counter++] = String.fromCharCode(getNumber(65, 90));
+    }
+    //Number
+    if (number) {
+      for (var i = 0; i < requirementLength && counter < length; i++)
+        arr[counter++] = String.fromCharCode(getNumber(48, 57));
+    }
+    //Special character
+    if (specialChar) {
+      for (var i = 0; i < requirementLength && counter < length; i++)
+        arr[counter++] = String.fromCharCode(getNumber(33, 47));
+    }
+    //Lower Case
+    if (lowerCase) {
+      for (var i = 0; i < requirementLength && counter < length; i++)
+        arr[counter++] = String.fromCharCode(getNumber(97, 122));
+    }
+  }
+
   return arr.join('');
 }
 
 function writePassword() {
   //
   var lengthOfPassword = prompt("Enter the length of the number between 8-128 character:");
-  var upperCase = confirm ("Do you want uppercase letter?");
-  var lowerCase = confirm ("Do you want lowercase letter?");
-  var number = confirm ("Do you want number to include in your password?");
-  var specialChar = confirm ("Do you want specialChar in your password?o");
-  var length_value= parseInt(lengthOfPassword,10);
-  document.querySelector("#password").textContent="";
-  if (isNaN(length_value) || length_value < 8 || length_value > 128) {
-   alert("Invalid number entered");
+  var upperCase = confirm("Do you want uppercase letter?");
+  var lowerCase = confirm("Do you want lowercase letter?");
+  var number = confirm("Do you want number to include in your password?");
+  var specialChar = confirm("Do you want specialChar in your password?o");
+  var length = parseInt(lengthOfPassword, 10);
+  document.querySelector("#password").textContent = "";
+  if (isNaN(length) || length < 8 || length > 128) {
+    alert("Invalid number entered");
   }
-  else if (upperCase || lowerCase || number || specialChar){    
-    document.querySelector("#password").textContent= generatePassword(length_value) ;
+  else if (upperCase || lowerCase || number || specialChar) {
+    document.querySelector("#password").textContent = generatePassword(length, upperCase, lowerCase, number, specialChar);
   }
-  else
-  {
+  else {
     alert("requirement not met");
-  } 
+  }
 }
 
 
